9632a02eecb298c298ebeb59444fe00fcfa9a6df
HakuRan <WBhlietue@gmail.com>
2026-05-15 10:36:36 +0800

    es

--- app/api/payment/qpay/route.ts
+++ app/api/payment/qpay/route.ts
@@ -1,32 +1,52 @@
-import { NextRequest } from "next/server";
+  import { NextRequest } from "next/server";
 import { createInvoice, QPayError } from "@/lib/qpay-client";
+import { jsonData } from "../../countryData";
+import { listPackages } from "@/lib/esim-client";
 
 export async function POST(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const { orderId, amount, description } = body;
+    
+    
+    try {
+        const body = await request.json();
+        const { orderId, slug } = body;
 
-    if (!orderId || !amount) {
-      return Response.json(
-        { success: false, message: "orderId and amount are required" },
-        { status: 400 }
-      );
+        if (!orderId || !slug) {
+            return Response.json(
+                { success: false, message: "orderId and slug are required" },
+                { status: 400 }
+            );
+        }
+        const counrty = slug.split("_")[0];
+        const data = jsonData[counrty].prices.find((x) => x.slug === slug);
+
+        if (!data) {
+            throw new QPayError(400, "invalid_slug");
+        }
+        const remoteData = await listPackages({ slug: slug });
+        if (!remoteData) {
+            throw new QPayError(400, "invalid_slug esim");
+        }
+
+        const invoice = await createInvoice({
+            orderId,
+            amount: data.price,
+            description: remoteData.packageList[0].description,
+        });
+
+        return Response.json({ success: true, invoice });
+    } catch (err) {
+        console.log(err);
+
+        if (err instanceof QPayError) {
+            return Response.json(
+                { success: false, code: err.code, message: err.message },
+                { status: 400 }
+            );
+        }
+        console.error("[/api/payment/qpay] Error:", err);
+        return Response.json(
+            { success: false, message: "Internal server error" },
+            { status: 500 }
+        );
     }
-
-    const invoice = await createInvoice({ orderId, amount, description });
-
-    return Response.json({ success: true, invoice });
-  } catch (err) {
-    if (err instanceof QPayError) {
-      return Response.json(
-        { success: false, code: err.code, message: err.message },
-        { status: 400 }
-      );
-    }
-    console.error("[/api/payment/qpay] Error:", err);
-    return Response.json(
-      { success: false, message: "Internal server error" },
-      { status: 500 }
-    );
-  }
 }
